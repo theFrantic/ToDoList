d2af2a7adc8c62cb130c4ec8881387e318d339f2
Daniel Whilchy <danielwhilchy@daniels-mac.creativesystems.lan>
2013-11-29 08:57:00 -0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Daniel Whilchy on 29/11/13.
+//  Copyright (c) 2013 Daniel Whilchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Daniel Whilchy on 29/11/13.
+//  Copyright (c) 2013 Daniel Whilchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
